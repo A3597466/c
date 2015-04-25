9f75530186c7049515325fe5c6ecde74956fa187
Sunny <Sunny@SunnydeMacBook-Pro.local>
2015-04-25 13:42:01 +0800

    Initial Commit

---
+++ HelloC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloC
+//
+//  Created by Sunny on 15/4/25.
+//  Copyright (c) 2015年 Sunny. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
